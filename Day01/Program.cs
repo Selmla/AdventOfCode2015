93c810761818c9caaf05e2f1dcae96560403c5f7
Nayomi <nayomi.arvell@gmail.com>
2025-10-28 16:33:56 +0100

    Initial

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,20 +4,52 @@
     {
         var input = File.ReadAllText("input.txt");
 
-        int floor = 0;
+        int part1 = SolvePart1(input);
+        int part2 = SolvePart2(input);
 
-        foreach (char c in input)
+        Console.WriteLine($"Part 1, end floor: {part1}");
+        Console.WriteLine($"Part 2, cellar position: {part2}");
+
+
+        static int SolvePart1(string input)
         {
-            if (c == '(')
+
+            int floor = 0;
+
+            foreach (char c in input)
             {
-                floor++;
+                if (c == '(')
+                {
+                    floor++;
+                }
+                else if (c == ')')
+                {
+                    floor--;
+                }
             }
-            else if (c == ')')
-            {
-                floor--;
-            }
+
+            return floor;
         }
 
-        Console.WriteLine($"Final floor: {floor}");
+        static int SolvePart2(string input)
+        {
+            int floor = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (c == '(') floor++;
+                else if (c == ')') floor--;
+
+                if (floor < 0)
+                {
+                    return i + 1; // Return position (1-based index)
+                }
+
+            }
+
+            return -1; // If basement is never reached
+        }
     }
 }
