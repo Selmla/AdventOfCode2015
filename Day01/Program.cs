3ea9007c30ec7f2a6d0a5f20305c581065def2cf
Nayomi <nayomi.arvell@gmail.com>
2025-10-28 14:36:45 +0100

    Add Day01 Solution

---
+++ Day01/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var input = File.ReadAllText("input.txt");
+
+        int floor = 0;
+
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                floor++;
+            }
+            else if (c == ')')
+            {
+                floor--;
+            }
+        }
+
+        Console.WriteLine($"Final floor: {floor}");
+    }
+}
